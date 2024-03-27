87bd49c8488131cceca705add6673c7f5fa2452b
mitchinnn <s22110054@student.unklab.ac.id>
2024-03-27 20:44:38 +0800

    adding exe6

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+
+const Title = () => {
+    return(
+        <View>
+            <Text>Basic Component in RN</Text>
+        </View>
+    )
+}
+
+export default Title;
